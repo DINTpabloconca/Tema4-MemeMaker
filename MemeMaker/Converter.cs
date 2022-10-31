4e93df4f99c31f8dcf0737623b0dd7e736284f57
pabloconca <pabloconcasanchez@gmail.com>
2022-10-31 16:59:14 +0100

    Ejercicio acabado

---
+++ MemeMaker/Converter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MemeMaker
+{
+    class Converter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool check = (bool)value;
+
+            if (check)
+                return new Thickness(2);
+            else
+                return new Thickness(0);
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
